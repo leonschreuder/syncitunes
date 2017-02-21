7709020c3d54b3b7494733906d76bd9c8167c323
meonlol <mail@leonmoll.de>
2017-02-21 20:32:44 +0100

    cleanup applescript interface

--- itunes-boundry/itunes_interface_applescript.go
+++ itunes-boundry/itunes_interface_applescript.go
@@ -19,52 +19,44 @@ func (a applescriptInterface) newFolder(name string) string {
 }
 
 func (a applescriptInterface) newPlaylist(name, parentID string) string {
-	var output []byte
+	var output string
 	if parentID != "" {
 		output, _ = a.runAppleScriptForItunes(`return id of (make new user playlist at playlist id ` + parentID + ` with properties {name:"` + name + `"})`)
 	} else {
 		output, _ = a.runAppleScriptForItunes(`return id of (make new user playlist with properties {name:"` + name + `"})`)
 	}
-	return strings.TrimSpace(string(output))
+	return output
 }
 
 func (a applescriptInterface) getPlaylistIDByName(name string) (string, error) {
-	requestOutput, err := a.runAppleScriptForItunes(
-		`return id in playlist "` + name + `"`,
-	)
-	stdOut := strings.TrimSpace(string(requestOutput))
-	if err != nil {
-		return "", errors.New(stdOut)
-	}
-	return stdOut, err
+	return a.runAppleScriptForItunes(`return id in playlist "` + name + `"`)
 }
 
 func (a applescriptInterface) getParentIDForPlaylist(id string) (string, error) {
-	requestOutput, err := a.runAppleScriptForItunes(
-		`return id of parent in playlist id ` + id,
-	)
+	return a.runAppleScriptForItunes(`return id of parent in playlist id ` + id)
+}
+
+func (a applescriptInterface) addFileToPlaylist(filePath, playlistID string) error {
+	_, err := a.runAppleScriptForItunes(`add POSIX file "` + filePath + `" to user playlist id ` + playlistID)
+	return err
+}
+
+func (a applescriptInterface) deletePlaylistByID(id string) error {
+	_, err := a.runAppleScriptForItunes(`delete playlist id ` + id)
+	return err
+}
+
+func (a applescriptInterface) runAppleScriptForItunes(commandLines ...string) (string, error) {
+	iTunesLines := []string{`tell application "iTunes"`}
+	iTunesLines = append(iTunesLines, commandLines...)
+	iTunesLines = append(iTunesLines, `end tell`)
+
+	requestOutput, err := a.runAppleScript(iTunesLines...)
 	stdOut := strings.TrimSpace(string(requestOutput))
 	if err != nil {
 		return "", errors.New(stdOut)
 	}
 	return stdOut, err
-}
-
-func (a applescriptInterface) addFileToPlaylist(filePath, playlistID string) {
-	a.runAppleScriptForItunes(
-		`add POSIX file "` + filePath + `" to user playlist id ` + playlistID,
-	)
-}
-
-func (a applescriptInterface) deletePlaylistByID(id string) {
-	a.runAppleScriptForItunes(`delete playlist id ` + id)
-}
-
-func (a applescriptInterface) runAppleScriptForItunes(commandLines ...string) ([]byte, error) {
-	iTunesLines := []string{`tell application "iTunes"`}
-	iTunesLines = append(iTunesLines, commandLines...)
-	iTunesLines = append(iTunesLines, `end tell`)
-	return a.runAppleScript(iTunesLines...)
 }
 
 func (a applescriptInterface) runAppleScript(commandLines ...string) ([]byte, error) {
