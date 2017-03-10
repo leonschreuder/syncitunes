d02a9b880c621c69df15193048c3436be43760f7
meonlol <mail@leonmoll.de>
2017-03-10 20:30:29 +0100

    First version of logic to add files to iTunes

--- syncitunes.go
+++ syncitunes.go
@@ -1,20 +1,23 @@
 package main
 
 type itunesInterface interface {
-	newFolder(name string) string
-	newPlaylist(name, parentID string) string
-	getPlaylistIDByName(name string) (string, error)
-	getParentIDForPlaylist(id string) (string, error)
-	addFileToPlaylist(filePath, playlistID string) error
-	deletePlaylistByID(id string) error
+	newFolder(name string, id int) int
+	newPlaylist(name string, parentID int) int
+	getPlaylistIDByName(name string) (int, error)
+	getParentIDForPlaylist(id int) (int, error)
+	addFileToPlaylist(filePath string, playlistID int) error
+	deletePlaylistByID(id int) error
 }
 
 var iTunes itunesInterface
 var root = ""
 
-func createPlaylist(file string) string {
-	parentID := iTunes.newFolder("someFolder")
-	id := iTunes.newPlaylist("itunes-boundry", parentID)
-	iTunes.addFileToPlaylist(file, id)
-	return id
+func fileTreeToItunes(node *node) {
+	r := iTunes.newFolder(node.name, 0)
+	for _, n := range node.nodes {
+		r2 := iTunes.newPlaylist(n.name, r)
+		// r2 := iTunes.newFolder(n.name, r)
+		// iTunes.newPlaylist(n.nodes[0].name, r2)
+		iTunes.addFileToPlaylist(n.nodes[0].path, r2)
+	}
 }
--- syncitunes_test.go
+++ syncitunes_test.go
@@ -1,65 +1,127 @@
 package main
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test__sould_be_able_to_create_playlist_with_file(t *testing.T) {
+var count = 0
+
+func Test__tree_with_single_file_to_itunes(t *testing.T) {
+	fileTree = &node{}
+	cwd = ""
 	mock := mockInterface{}
 	iTunes = &mock
+	resultNode = &mockNode{}
 
-	wd, _ := os.Getwd()
-	root = wd
-	absFile := wd + "/itunes-boundry/empty.mp3"
-	id := createPlaylist(absFile)
+	addFileToTree("root/some_album/song.mp3")
 
-	assert.Equal(t, "1337", id)
-	assert.Equal(t, "itunes-boundry", mock.playlistName)
-	assert.Equal(t, absFile, mock.file)
+	fileTreeToItunes(fileTree)
+
+	assert.Equal(t, "root", resultNode.name)
+	assert.EqualValues(t, d, resultNode.kind)
+	assert.Equal(t, 1, len(resultNode.mockNodes))
+	assert.Equal(t, "some_album", resultNode.mockNodes[0].name)
+	assert.EqualValues(t, p, resultNode.mockNodes[0].kind)
+	assert.Equal(t, 1, len(resultNode.mockNodes[0].mockNodes))
+	assert.EqualValues(t, f, resultNode.mockNodes[0].mockNodes[0].kind)
+	assert.Equal(t, "root/some_album/song.mp3", resultNode.mockNodes[0].mockNodes[0].name)
 }
 
-func Test__sould_be_able_to_create_playlist_in_folder(t *testing.T) {
+func Test__tree_with_multiple_files_to_itunes(t *testing.T) {
+	fileTree = &node{}
+	cwd = ""
 	mock := mockInterface{}
 	iTunes = &mock
+	resultNode = &mockNode{}
 
-	wd, _ := os.Getwd()
-	root = wd
-	absFile := wd + "/someFolder/itunes-boundry/empty.mp3"
-	createPlaylist(absFile)
+	addFileToTree("root/some_album/song.mp3")
+	addFileToTree("root/some_other_album/song.mp3")
 
-	assert.Equal(t, "itunes-boundry", mock.playlistName)
-	assert.Equal(t, "someFolder", mock.folderCreated)
-	assert.Equal(t, absFile, mock.file)
+	fileTreeToItunes(fileTree)
+
+	assert.Equal(t, "root", resultNode.name)
+	assert.EqualValues(t, d, resultNode.kind)
+	assert.Equal(t, 2, len(resultNode.mockNodes))
+	assert.EqualValues(t, p, resultNode.mockNodes[0].kind)
+	assert.Equal(t, "some_album", resultNode.mockNodes[0].name)
+	assert.EqualValues(t, p, resultNode.mockNodes[1].kind)
+	assert.Equal(t, "some_other_album", resultNode.mockNodes[1].name)
+	assert.Equal(t, 1, len(resultNode.mockNodes[0].mockNodes))
+	assert.EqualValues(t, f, resultNode.mockNodes[0].mockNodes[0].kind)
+	assert.Equal(t, "root/some_album/song.mp3", resultNode.mockNodes[0].mockNodes[0].name)
+	assert.EqualValues(t, f, resultNode.mockNodes[1].mockNodes[0].kind)
+	assert.Equal(t, "root/some_other_album/song.mp3", resultNode.mockNodes[1].mockNodes[0].name)
+}
+
+var resultNode *mockNode
+
+type itemType int
+
+type mockNode struct {
+	name      string
+	id        int
+	parent    int
+	kind      itemType
+	mockNodes []*mockNode
+}
+
+const (
+	f = iota //file
+	d        //dir
+	p        //playlist
+)
+
+func addNode(name string, t itemType, parent int) int {
+	count++
+	newNode := &mockNode{name: name, id: count, kind: t}
+	if resultNode.name == "" {
+		resultNode.name = newNode.name
+		resultNode.id = newNode.id
+		resultNode.kind = newNode.kind
+	} else {
+		if resultNode.id == parent {
+			resultNode.mockNodes = append(resultNode.mockNodes, newNode)
+		} else {
+			for _, n := range resultNode.mockNodes {
+				if n.id == parent {
+					n.mockNodes = append(n.mockNodes, newNode)
+				}
+			}
+		}
+	}
+	return count
 }
 
 type mockInterface struct {
-	file          string
-	playlistName  string
-	folderCreated string
+	currentID   int
+	pathCreated string
 }
 
-func (i *mockInterface) newFolder(name string) string {
-	i.folderCreated = name
-	return ""
+func (i *mockInterface) newFolder(name string, id int) int {
+	// newNode := &mockNode{name: name, id: id + 1, kind: "f"}
+	return addNode(name, d, id)
+	// return id + 1
 }
-func (i *mockInterface) newPlaylist(name, parentID string) string {
-	i.playlistName = name
-	return "1337"
+func (i *mockInterface) newPlaylist(name string, id int) int {
+	// newNode := &mockNode{name: name, id: id + 1}
+	// addNode(newNode, id)
+	// return newNode.id
+	return addNode(name, p, id)
+	// return id + 1
 }
-func (mockInterface) getPlaylistIDByName(name string) (string, error) {
-	return "", nil
+func (mockInterface) getPlaylistIDByName(name string) (int, error) {
+	return -1, nil
 }
-func (mockInterface) getParentIDForPlaylist(id string) (string, error) {
-	return "", nil
+func (mockInterface) getParentIDForPlaylist(id int) (int, error) {
+	return -1, nil
 }
-func (i *mockInterface) addFileToPlaylist(filePath, playlistID string) error {
-	i.file = filePath
+func (i *mockInterface) addFileToPlaylist(filePath string, playlistID int) error {
+	addNode(filePath, f, playlistID)
 	return nil
 }
 
-func (mockInterface) deletePlaylistByID(id string) error {
+func (mockInterface) deletePlaylistByID(id int) error {
 	return nil
 }
