3bc13b3e2516dc5bdcd854779aa4bfc9f142ec86
meonlol <mail@leonmoll.de>
2017-06-18 20:52:58 +0200

    Hectic resturcturing of lexer. Commiting pre-refactor

--- itunes/applescript_cli/parser/lexer.go
+++ itunes/applescript_cli/parser/lexer.go
@@ -12,15 +12,18 @@ type item struct {
 }
 
 const (
-	itemError  itemType = iota // error occured. Value is error text
-	plainText                  //
-	opener                     // {
-	openQuote                  // "
-	closeQuote                 // "
-	closer                     // }
-	itemName                   //name of the playlist
-	itemId                     //name of the playlist
-	separator                  // ',' with or without ' '
+	itemError      itemType = iota // error occured. Value is error text
+	plainText                      //
+	opener                         // {
+	openQuote                      // "
+	closeQuote                     // "
+	closer                         // }
+	itemName                       // name of the playlist
+	itemId                         // id of the playlist
+	separator                      // ',' with or without ' '
+	aliasIndicator                 // word alias
+	filePathOpener                 // "
+	filePath                       // the path of the file
 )
 
 type lexer struct {
@@ -41,7 +44,7 @@ func lex(input string) *lexer {
 }
 
 func (l *lexer) run() {
-	for state := lexText; state != nil; {
+	for state := lexInputString; state != nil; {
 		state = state(l)
 	}
 	close(l.items)
@@ -57,11 +60,12 @@ func (l *lexer) emit(t itemType) {
 	l.start = l.pos
 }
 
-func lexText(l *lexer) stateFn {
+func lexInputString(l *lexer) stateFn {
 	for {
 		if peekNext(l) == "{" {
-			l.emit(plainText)
-			return lexOpener
+			l.pos++
+			l.emit(opener)
+			return lexObjectArray
 		}
 		l.pos++
 	}
@@ -71,8 +75,38 @@ func lexText(l *lexer) stateFn {
 func lexOpener(l *lexer) stateFn {
 	l.pos++
 	l.emit(opener)
-	return lexInsideObject
+	return lexObjectArray
 }
+
+func lexObjectArray(l *lexer) stateFn {
+	for {
+		if l.pos < len(l.input) {
+			switch peekNext(l) {
+			case "{":
+				l.pos++
+				l.emit(opener)
+				return lexInsideObject
+			case "}":
+				l.pos++
+				l.emit(closer)
+			case ",":
+				l.pos++
+				l.emit(separator)
+			}
+			l.pos++
+			l.start++
+		} else {
+			break
+		}
+	}
+	return nil
+}
+
+// func lexObjectOpener(l *lexer) stateFn {
+// 	l.pos++
+// 	l.emit(opener)
+// 	return lexInsideObject
+// }
 
 func lexCloser(l *lexer) stateFn {
 	l.pos++
@@ -81,27 +115,130 @@ func lexCloser(l *lexer) stateFn {
 }
 
 func lexInsideObject(l *lexer) stateFn {
-	if l.pos < len(l.input) {
-		switch peekNext(l) {
-		case "{":
-			return lexOpener
-		case "}":
-			return lexCloser
-		case "\"":
-			return lexOpenQuote
-		case ",":
-			return lexSeparator
-		case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
-			return lexID
+	for {
+		if l.pos < len(l.input) {
+			switch peekNext(l) {
+			case "{":
+				return lexFilesListOpener
+			case "}":
+				l.pos++
+				l.emit(closer)
+				return lexObjectArray
+			case "\"":
+				l.pos++
+				l.emit(openQuote)
+				return lexInsideString
+			case ",":
+				l.pos++
+				l.emit(separator)
+			// return lexInsideObject
+			// 	return lexSeparator
+			case "-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+				return lexID
+			}
 		}
+		l.pos++
+		l.start++
 	}
 	return nil
+}
+
+func lexSeparator(l *lexer) stateFn {
+	for {
+		if peekNext(l) != " " && peekNext(l) != "," {
+			l.emit(separator)
+			return lexInsideObject
+		}
+		l.pos++
+	}
 }
 
 func lexOpenQuote(l *lexer) stateFn {
 	l.pos++
 	l.emit(openQuote)
 	return lexInsideString
+}
+
+func lexID(l *lexer) stateFn {
+	for {
+		nxt := peekNext(l)
+		if !(unicode.IsDigit([]rune(nxt)[0]) || nxt == "-") {
+			l.emit(itemId)
+			return lexInsideObject
+		}
+		l.pos++
+	}
+}
+
+func lexFilesListOpener(l *lexer) stateFn {
+	l.pos++
+	l.emit(opener)
+	return lexInsideFilesList
+}
+
+func lexInsideFilesList(l *lexer) stateFn {
+	if l.pos < len(l.input) {
+		switch peekNext(l) {
+		case "a":
+			return lexAliasNotation
+		case "\"":
+			return lexFilePathCloser
+		case ",":
+			return lexSeparator
+		case "}":
+			return lexCloser
+		}
+	}
+	return nil
+}
+
+func lexAliasNotation(l *lexer) stateFn {
+	for {
+		// Expecting the word 'alias' followed by a space
+		if peekNext(l) == " " {
+			l.emit(aliasIndicator)
+			return lexFilePathOpener
+		}
+		l.pos++
+	}
+}
+
+func lexFilePathOpener(l *lexer) stateFn {
+	for {
+		// Expecting the word 'alias' followed by a space
+		if peekNext(l) != " " && peekNext(l) != "\"" {
+			l.emit(filePathOpener)
+			return lexFilePath
+		}
+		l.pos++
+	}
+	l.pos++
+	l.emit(filePathOpener)
+	return lexInsideString
+}
+
+func lexFilePath(l *lexer) stateFn {
+	for {
+		// Scanning path until closing quote.
+		// TODO: Should be able to handle quotes inside filenames
+		if peekNext(l) == "\"" {
+			l.emit(filePath)
+			return lexInsideFilesList
+		}
+		l.pos++
+	}
+}
+
+func lexFilePathCloser(l *lexer) stateFn {
+	l.pos++
+	l.emit(closeQuote)
+	return lexFilesListCloser
+}
+
+func lexFilesListCloser(l *lexer) stateFn {
+	l.pos++
+	l.emit(closer)
+	return lexInsideObject
 }
 
 func lexCloseQuote(l *lexer) stateFn {
@@ -115,29 +252,9 @@ func lexInsideString(l *lexer) stateFn {
 		switch peekNext(l) {
 		case "\"":
 			l.emit(itemName)
-			return lexCloseQuote
-		}
-		l.pos++
-	}
-	return nil
-}
 
-func lexSeparator(l *lexer) stateFn {
-	for {
-		if peekNext(l) != " " && peekNext(l) != "," {
-			// l.pos++ //we want to include the current rune
-			l.emit(separator)
-			return lexInsideObject
-		}
-		l.pos++
-	}
-	return nil
-}
-
-func lexID(l *lexer) stateFn {
-	for {
-		if !unicode.IsDigit([]rune(peekNext(l))[0]) {
-			l.emit(itemId)
+			l.pos++
+			l.emit(closeQuote)
 			return lexInsideObject
 		}
 		l.pos++
